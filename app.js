bf61aafdf68f64a791b1ab7cc1eba6a0f7f65250
momosuke <rusk.ryoy4n@gmail.com>
2023-10-05 23:51:32 +0900

    update: timeout

--- app.js
+++ app.js
@@ -83,8 +83,8 @@ async function tweetClip(data, title, url) {
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+server.keepAliveTimeout = 180 * 1000;
+server.headersTimeout = 180 * 1000;
 
 const html = `
 <!DOCTYPE html>
